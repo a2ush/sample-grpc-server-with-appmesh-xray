c74c6e7abbd5c1120fa1ca3221ba56d94ef28ecc
a2ush <azush@amazon.com>
2022-02-14 09:46:23 +0900

    Client can access grpc server whereever server is.

--- grpc/client/client.go
+++ grpc/client/client.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/a2ush/sample-grpc-server-with-appmesh-xray/rpc"
@@ -16,28 +16,42 @@ func main() {
 
 	log.Print("HTTP Server is running with :8080 port...")
 
-	http.HandleFunc("/jst", getJSTTime)
-	http.HandleFunc("/utc", getUTCTime)
+	http.HandleFunc("/", getTime)
 	http.ListenAndServe(":8080", nil)
 }
 
-func getJSTTime(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		marshaled_result, _ := json.Marshal(gRPCRequest("JST"))
-		fmt.Fprintf(w, string(marshaled_result))
-	}
-}
+func getTime(w http.ResponseWriter, r *http.Request) {
 
-func getUTCTime(w http.ResponseWriter, r *http.Request) {
+	log.Println(r)
 	if r.Method == http.MethodGet {
-		marshaled_result, _ := json.Marshal(gRPCRequest("UTC"))
-		fmt.Fprintf(w, string(marshaled_result))
+
+		var marshaled_result []byte
+
+		switch r.RequestURI {
+		case "/utc":
+			marshaled_result, _ = json.Marshal(gRPCRequest("UTC"))
+		case "/pst":
+			marshaled_result, _ = json.Marshal(gRPCRequest("PST"))
+		case "/jst":
+			marshaled_result, _ = json.Marshal(gRPCRequest("JST"))
+		default:
+			marshaled_result, _ = json.Marshal(gRPCRequest("UTC"))
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(marshaled_result)
 	}
 }
 
 func gRPCRequest(timezone string) *rpc.ServerResponse {
 
-	address := "localhost:50051"
+	path, found := os.LookupEnv("SERVER_PATH")
+	if !found {
+		path = "localhost"
+	}
+	address := path + ":50051"
+
 	conn, err := grpc.Dial(
 		address,
 		grpc.WithInsecure(),
@@ -67,13 +81,17 @@ func gRPCRequest(timezone string) *rpc.ServerResponse {
 		req = rpc.ClientRequest{
 			TimezoneFormat: rpc.Timezone_UTC,
 		}
+	case "PST":
+		req = rpc.ClientRequest{
+			TimezoneFormat: rpc.Timezone_PST,
+		}
 	}
 
-	reply, err := client.ConvertTime(ctx, &req)
+	result, err := client.ConvertTime(ctx, &req)
 	if err != nil {
 		log.Fatal("Request failed.")
 		return nil
 	}
 
-	return reply
+	return result
 }
